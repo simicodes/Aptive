d09394a56bead2f496a48b8789eb1e7bac89b90a
simicodes <simeonoffiong@gmail.com>
2022-07-01 03:33:31 -0600

    Pricing component updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.css";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Pricing from "./Pricing";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Pricing />
     </div>
   );
 };
